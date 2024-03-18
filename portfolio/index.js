fdf6f73a6a5f6d18c13fe5e396525cb1098bed8e
pjeyaprakash <p.jeyaprakashraja000@gmail.com>
2024-03-18 08:44:09 +0530

    index.js file updated

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,35 +1,24 @@
 let l=0
 let a=0
 let pi=0
-
 window.addEventListener('scroll',()=>{
     let c=window.pageYOffset
-    console.log(c)
     if(c>1300 && a==0){
         document.querySelector(".tech").classList.add("anicltech")
         document.querySelector(".soft").classList.add("aniclsoft")
-        console.log("skill")
         a++
     }
     if (l>c){
         document.querySelector("header").style.top="0px"
         document.querySelector("header").style.boxShadow="10px 0px 15px"
-        console.log("up")
     }
     else{
         document.querySelector("header").style.top="-101px"
         document.querySelector("header").style.boxShadow="10px 20px 25px rgb(203, 252, 255)"
-        console.log("down")
     }
-
     l=c
-
     if (c>2100 && pi==0){
-        console.log("p")
         document.querySelector("#preo").classList.add("preoc")
         pi++
     }
-    
 })
-
-document.querySelector("header").classList.remove()
